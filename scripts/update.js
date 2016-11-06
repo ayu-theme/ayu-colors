322d18af586ec8a40be331f08510ee5664b3ce62
Konstantin Pschera <me@kons.ch>
2016-11-06 22:18:44 +0100

    Update licensing
    @dempfi's license is now prepended to all color files

--- scripts/update.js
+++ scripts/update.js
@@ -8,6 +8,32 @@ import camelCase from 'lodash.camelcase'
 import mapObject from 'map-obj'
 import isObject from 'is-object'
 
+const license = `
+/**
+ * MIT License
+ *
+ * Copyright (c) 2016 Ike Ku
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+`
+
 const camelCaseKeys = (obj) => mapObject(obj, (key, value) => {
 	return [
 		camelCase(key),
@@ -15,7 +41,7 @@ const camelCaseKeys = (obj) => mapObject(obj, (key, value) => {
 	]
 })
 
-const template = (colors) => `export default ${JSON.stringify(camelCaseKeys(colors), null, '\t')}`
+const template = (colors) => `${license}\nexport default ${JSON.stringify(camelCaseKeys(colors), null, '\t')}`
 
 const clone = spawn('git', ['clone', 'https://github.com/dempfi/ayu'])
 
